855ca00538cd10d21a11636b0b5196918ed37c72
Victor Coelho <victor09120@gmail.com>
2018-12-07 16:37:44 -0200

    Started project. Created grid with CSS grid. Updated README.md file.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# etch-a-sketch
+# Etch-a-Sketch Assignment
+This is a repository for the Etch-a-Sketch assignment from The Odin Project Web Dev 101 course.
---
+++ main.js
@@ -0,0 +1,24 @@
+/* let gridRow = document.createElement("div");
+gridRow.classList.toggle("row");
+let gridColumn = document.createElement("div");
+gridColumn.classList.toggle("column");
+gridRow.appendChild(gridColumn);
+document.body.appendChild(gridRow); */
+
+let grid = document.querySelector(".grid");
+for(let i = 0; i < 16; i++) {
+    let gridRow = document.createElement("div");
+    gridRow.classList.toggle("row");
+    for(let j = 0; j < 16; j++) {
+        let gridColumn = document.createElement("div");
+        gridColumn.classList.toggle("column");
+        gridRow.appendChild(gridColumn);
+    }
+    grid.appendChild(gridRow);
+}
+
+document.addEventListener("mouseover", function(event) {
+    if(event.target.matches(".column")) {
+        console.log(this);
+    }
+})
